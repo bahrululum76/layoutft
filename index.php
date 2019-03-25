aabf813e5cfcff0b774bbe6521f1ab21c5778c30
ratusyqna <bahrululum756@gmail.com>
2019-03-25 19:39:19 +0700

    comit satu

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Fakultas Teknik Universitas Suryakancana</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/solid.css" integrity="sha384-r/k8YTFqmlOaqRkZuSiE9trsrDXkh07mRaoGBMoDcmA58OHILZPsk29i2BsFng1B" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/regular.css" integrity="sha384-IG162Tfx2WTn//TRUi9ahZHsz47lNKzYOp0b6Vv8qltVlPkub2yj9TVwzNck6GEF" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/brands.css" integrity="sha384-BKw0P+CQz9xmby+uplDwp82Py8x1xtYPK3ORn/ZSoe6Dk3ETP59WCDnX+fI1XCKK" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css" integrity="sha384-4aon80D8rXCGx9ayDt85LbyUHeMWd3UiBaWliBlJ53yzm9hqN21A+o1pqoyK04h+" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    
+  </head>
+  <body>
+
+    <header>
+    <div>
+      
+    </div>
+    <div>
+      
+    </div>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top ">
+        <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="" /></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php"><i class="glyphicon glyphicon-home"></i> Beranda <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profil</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01" style="background-color:rgba(255,255,255,.5);">
+                <a class="dropdown-item" href="?page=sambutan">Sambutan</a>
+                <a class="dropdown-item" href="#">Sejarah</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Program Studi</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown02" style="background-color:rgba(255,255,255,.5);">
+                <a class="dropdown-item" href="#">Teknik Industri</a>
+                <a class="dropdown-item" href="#">Teknik Informatika</a>
+                <a class="dropdown-item" href="#">Teknik Sipil</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">E-Dokumen</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Berita</a>
+            </li>
+          </ul>
+            <a href="signin.php"><button class="btn btn-outline-success my-2 my-sm-0">Login</button></a>            
+        </div>
+      </nav>
+    </header>
+
+
+<div class="container-fluid"  >
+<?php 
+include 'conten.php';?>
+</div> 
+
+      <!-- FOOTER -->
+    <footer class="page-footer ">
+        <br>  
+        <p class="float-right"><a href="#">Kembali Ke Atas</a></p>
+        <center> <font color="white"> <p>&copy; 2017 Fakultas Teknik Universitas Suryakancana.</p></font></center>
+        <br>  
+      </footer>
+
+      
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
